fce5b0e1c42224a60fa976a18dcdf21046adf3d0
terichadbourne <terichad@gmail.com>
2018-08-07 20:55:03 -0400

    Add `discussion-form` component and enable update
    - Add discussion-form template called from discussion with action
    `toggleEditing` passed as `cancel` and `updateDiscussion` passed as is
    - `Save item` button calls `updateDiscussion` action passed up through
    route to update discussion in database
    - `Cancel` button triggers `toggleEditing` to hide the form and show the
    uneditable title again

---
+++ tests/integration/components/propose-topics/discussion-list/discussion-form-test.js
@@ -0,0 +1,24 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('propose-topics/discussion-list/discussion-form', 'Integration | Component | propose topics/discussion list/discussion form', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+
+  this.render(hbs`{{propose-topics/discussion-list/discussion-form}}`);
+
+  assert.equal(this.$().text().trim(), '');
+
+  // Template block usage:
+  this.render(hbs`
+    {{#propose-topics/discussion-list/discussion-form}}
+      template block text
+    {{/propose-topics/discussion-list/discussion-form}}
+  `);
+
+  assert.equal(this.$().text().trim(), 'template block text');
+});
